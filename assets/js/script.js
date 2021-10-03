3ff8c810999accd15259f89c2ef5bf5210c68543
TJ James <jamestw13@gmail.com>
2021-10-03 15:17:44 -0500

    added commenting and minor aesthetic tweaks

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,17 @@
 
 /* Global Variables */
 
+// HTML Elements
 let userFormEl = document.querySelector("#user-form");
 let destinationInputEl = document.querySelector("#destination");
 let searchHistoryEl = document.querySelector("#search-history");
+let currentTitleEl = document.querySelector("#current-title");
 let currentContainerEl = document.querySelector("#current-container");
+let forecastTitleEl = document.querySelector("#forecast-title");
 let futureContainerEl = document.querySelector("#future-container");
 let cityNameEl = document.querySelector("#city-name");
+
+// Search history array
 let destinationSearchHistory = [];
 
 /* Start Event Handlers */
@@ -15,50 +20,58 @@ let destinationSearchHistory = [];
 // Search from text input
 let formSubmitHandler = (event) => {
 	event.preventDefault();
+
+	// Get value from search input
 	var destination = destinationInputEl.value.trim();
 
 	// Validate input
 	if (destination) {
+		// Send value to API calls
 		getWeatherData(destination);
 	} else {
 		alert("Please enter a destination.");
 	}
 };
+// Search bar event listener
 userFormEl.addEventListener("submit", formSubmitHandler);
 
 // Search from search history button
 let historyButtonHandler = (event) => {
+	// get value from button
 	var destination = event.target.getAttribute("data-destination");
 
-	if (destination) {
-		getWeatherData(destination);
-	}
+	// Send value to API calls
+	getWeatherData(destination);
 };
+// Search history event listener
 searchHistoryEl.addEventListener("click", historyButtonHandler);
 
 /* End Event Handlers */
 
 /* Start main functions */
 
+// Create a tile with weather information from OpenWeather API
 let buildTile = (data) => {
+	// Tile Element
 	let weatherTile = document.createElement("div");
 	weatherTile.setAttribute("class", "weather-tile");
 
-	// Date
+	// Date Element
 	let dateText = document.createElement("h4");
 	let date = moment(data.dt * 1000).format("ddd MMM D");
 	dateText.textContent = date;
 
-	// Weather Icon
+	// Weather icon Element
 	let conditionIcon = document.createElement("img");
 	conditionIconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
 	conditionIcon.setAttribute("src", conditionIconUrl);
 	conditionIcon.setAttribute("alt", data.weather[0].main);
 
+	// Weather Condtion Element
 	let conditionText = document.createElement("h4");
 	conditionText.textContent = `${data.weather[0].main}`;
 
-	// Temperature
+	// Temperature Element
 	let tempText = document.createElement("h4");
 	let temp = data.temp;
 	if (typeof data.temp === "object") {
@@ -66,20 +79,23 @@ let buildTile = (data) => {
 	}
 	tempText.textContent = `Temperature: ${Math.floor(temp)}\xB0F`;
 
-	// Humidity
+	// Humidity Element
 	let humidityText = document.createElement("h4");
 	humidityText.textContent = `Humidity: ${data.humidity}%`;
 
-	// Wind Speed
+	// Wind Speed Element
 	let windText = document.createElement("h4");
 	windText.textContent = `Wind: ${Math.floor(data.wind_speed)} MPH`;
 
+	// Append elements to tile element
 	weatherTile.appendChild(dateText);
 	weatherTile.appendChild(conditionIcon);
 	weatherTile.appendChild(conditionText);
 	weatherTile.appendChild(tempText);
 	weatherTile.appendChild(humidityText);
 	weatherTile.appendChild(windText);
+
+	// Add UV Index line if tile is for current date
 	if (typeof data.temp !== "object") {
 		// UV Index
 		let uviText = document.createElement("h4");
@@ -100,9 +116,11 @@ let displayWeatherData = (data) => {
 	futureContainerEl.innerHTML = "";
 
 	// current day
+	currentTitleEl.textContent = "Current Conditions";
 	currentContainerEl.appendChild(buildTile(data.current));
 
 	// future days
+	forecastTitleEl.textContent = "5-Day Forecast";
 	for (let i = 1; i < 6; i++) {
 		futureContainerEl.appendChild(buildTile(data.daily[i]));
 	}
@@ -110,13 +128,14 @@ let displayWeatherData = (data) => {
 
 // Make nested API calls to get weather data
 let getWeatherData = (destination) => {
+	// Clear input
 	destinationInputEl.value = "";
+
 	// API call to Mapquest to get latitude and longitude from generic place name
 	let latLngSearchApiUrl = `http://open.mapquestapi.com/geocoding/v1/address?key=pmTncUmE4WZvotxffzMXoDh0tdUGP9Vc&location=${destination}`;
 	fetch(latLngSearchApiUrl).then((resp1) => {
 		if (resp1.ok) {
 			resp1.json().then((geoData) => {
-				console.log(geoData);
 				let lat = geoData.results[0].locations[0].latLng.lat;
 				let lng = geoData.results[0].locations[0].latLng.lng;
 				let city = geoData.results[0].locations[0].adminArea5;
@@ -128,26 +147,34 @@ let getWeatherData = (destination) => {
 						"Your destination search may be too broad. Please enter more specific location information for results."
 					);
 				} else {
+					// update destination variable with validated search string
+					let destination = `${city}, ${state} ${country}`;
 					// API call to OpenWeather using latitude and longitude
 					var weatherSearchApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&exclude=minutely,hourly,alerts&units=imperial&appid=50357db12d6b05e74bc14dbfe097e5cb`;
 					fetch(weatherSearchApiUrl).then((resp2) => {
 						if (resp2.ok) {
 							resp2.json().then((data) => {
-								// Display city name
-								cityNameEl.textContent = ` for: ${city}, ${state} ${country}`.toUpperCase();
+								// Display city name in header
+								cityNameEl.innerHTML = ` for: ${destination}`;
+
 								// Send weather data to display functions
 								displayWeatherData(data);
+
 								// Save destination to localStorage
-
-								destination = destination.toUpperCase();
-
+								// Add to front of search history array
 								destinationSearchHistory.unshift(destination);
+								// Remove repeated searches from history
 								destinationSearchHistory = [...new Set(destinationSearchHistory)];
+
+								// Limit of 10 searches in history - pop extra
 								while (destinationSearchHistory.length > 10) {
 									destinationSearchHistory.pop();
 								}
-								updateSearchHistory();
 
+								// Update search history Bar
+								updateSearchHistoryBar();
+
+								// Update localstorage
 								localStorage.setItem("destinationSearchHistory", JSON.stringify(destinationSearchHistory));
 							});
 						}
@@ -160,22 +187,32 @@ let getWeatherData = (destination) => {
 	});
 };
 
-let updateSearchHistory = () => {
+// Update search history bar
+let updateSearchHistoryBar = () => {
+	// clear old data
 	searchHistoryEl.innerHTML = "";
+
+	// loop over search history array
 	for (let item in destinationSearchHistory) {
+		// Make button element
 		let destButton = document.createElement("button");
 		destButton.setAttribute("class", "btn");
 		destButton.setAttribute("data-destination", destinationSearchHistory[item]);
 		destButton.innerHTML = destinationSearchHistory[item];
+
+		// Add button to search history bar
 		searchHistoryEl.appendChild(destButton);
 	}
 };
+
+// Load any search history array from localStorage onto screen
 let loadLocalStorage = () => {
 	let storedDestinations = localStorage.getItem("destinationSearchHistory");
 	if (storedDestinations) {
 		destinationSearchHistory = JSON.parse(storedDestinations);
 	}
-	updateSearchHistory();
+	updateSearchHistoryBar();
 };
 
+// Initiate page search history
 loadLocalStorage();
